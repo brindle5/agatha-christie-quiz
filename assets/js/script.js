4b664a765c353e2e83d28f9cfd4110db4696454f
brindle5 <sarah.brindle@hotmail.co.uk>
2022-12-06 12:16:21 +0000

    Add initial code for first question function

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let radio = document.getElementsByTagName('radio');
+
+let score;
+
+
+function questionOne() {
+   
+    let exotic = document.getElementById('exotic');
+    let mansion = document.getElementById('mansion');
+    let townhouse = document.getElementById('townhouse');
+
+    if (exotic) {
+        score = score + 5;
+    } else if (mansion) {
+        score = score + 10;
+    } else if (townhouse); {
+        score = score + 1;
+    }  
+
+}
+
+function questionTwo() {
+
+}
+
+function questionThree() {
+
+}
+
+function calculateRecommendation() {
+    if (score =  =< 14) {
+        recommendBookOne
+    } else if (score = >15) {
+        recommendBookTwo
+    }
+}
+
